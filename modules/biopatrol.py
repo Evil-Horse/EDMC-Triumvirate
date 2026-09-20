e438f2e046951b60df5aaf3a14e70a63378792a0
Evil Horse <rainbowdysch13@gmail.com>
2026-09-20 17:15:47 +0300

    fix cond

--- modules/biopatrol.py
+++ modules/biopatrol.py
@@ -1956,7 +1956,7 @@ class BioPatrol(tk.Frame, Module):
                 AND data_fss_body_signals.type = '$SAA_SignalType_Biological;'
                 GROUP BY data_bodies.system_id64, data_bodies.bodyid;
                 ''', (id64, body_id, self.cmdr_id)).fetchone()
-                if body_query is not None and body_query[3] < body_query[4]:
+                if body_query is not None and body_query[3] > body_query[4]:
                     message = f"{body_name}: отсутствует DSS"
                     debug(message)
                     if user_message is None:
